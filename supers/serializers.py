e0d5938e3252380db2730200580c02d0eabb0da7
Nathaniel-Mueller <nathaniel.15853.clsec.8@gmail.com>
2022-10-24 12:20:26 -0700

    Fixed nullified super_type foreign key, remade migrations

--- supers/serializers.py
+++ supers/serializers.py
@@ -2,7 +2,8 @@ from rest_framework import serializers
 from .models import Super
 
 class SuperSerializer(serializers.ModelSerializer):
+    super_type_id=serializers.IntegerField(write_only=True)
     class Meta:
         model = Super
-        fields = ['id','name','alter_ego','primary_ability','secondary_ability','catchphrase','super_type']
+        fields = ['id','name','alter_ego','primary_ability','secondary_ability','catchphrase','super_type','super_type_id']
         depth = 1
